b9acf7e7742f9f6ac073653cf1ac47b509516250
Luiz Fernando Arêas <lfernandoareas@gmail.com>
2021-08-16 05:23:37 -0300

    config eureka clients

--- aafunc/src/main/java/com/tehilim/aafunc/AafuncApplication.java
+++ aafunc/src/main/java/com/tehilim/aafunc/AafuncApplication.java
@@ -2,7 +2,9 @@ package com.tehilim.aafunc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AafuncApplication {
 
